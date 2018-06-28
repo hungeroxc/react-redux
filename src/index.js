bb8806d90c8b1daac753a18249feeddb75ef5b71
dachunchun <524112857@qq.com>
2018-06-28 15:49:53 +0800

    up 计数器

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ClickCounter from './ClickCounter'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <ClickCounter />, 
+    document.getElementById('root')
+)
